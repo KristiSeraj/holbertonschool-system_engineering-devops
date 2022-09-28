eb0bfe57b58e019f5d43dd4caaf54e93337235b9
KristiSeraj <serajkristi@gmail.com>
2022-09-28 06:23:11 -0700

    test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,16 +3,18 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=""):
     """Returns a list containing the title of all host articles"""
 
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url=url, headers=headers)
     if response.status_code == 404:
         return None
-    subs = response.json().get("data")
-    for post in subs.get("children"):
-        post_details = post.get("data")
-        hot_list.append(post_details.get("title"))
+    if after is None:
+        return hot_list
+    for post in response.json().get("data").get("children"):
+        hot_list.append(post.get("data").get("title"))
+    after = response.json().get("data").get("after")
+    recurse(subreddit, hot_list, after)
     return hot_list
